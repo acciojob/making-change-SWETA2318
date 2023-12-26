b174d50c677f7fe37474bfdb960549c59f5d4a56
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-26 09:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const makeChange = (c) => {
-  // your name here
+// Function to make change
+const makeChange = (cents) => {
+  // Initialize coin values
+  const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+  const pennyValue = 1;
+
+  // Initialize coin counts
+  let quarters = Math.floor(cents / quarterValue);
+  cents %= quarterValue;
+
+  let dimes = Math.floor(cents / dimeValue);
+  cents %= dimeValue;
+
+  let nickels = Math.floor(cents / nickelValue);
+  cents %= nickelValue;
+
+  let pennies = cents;
+
+  // Create and return the result object
+  const result = {
+    q: quarters,
+    d: dimes,
+    n: nickels,
+    p: pennies,
+  };
+
+  return result;
 };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// Prompt user for input
+const userInput = prompt("Enter c: ");
+const inputCents = parseInt(userInput);
+
+// Display the result
+alert(JSON.stringify(makeChange(inputCents)));
